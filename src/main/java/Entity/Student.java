52518b35deddb3040e926e05ec6ac053b1524aca
Migara Bandara <bandara-ps20045@stu.kln.ac.lk>
2024-11-10 17:25:02 +0530

    Builded

---
+++ src/main/java/Entity/Student.java
@@ -0,0 +1,39 @@
+package Entity;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "student")
+
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "first_name",nullable = false)
+    private String firstName;
+    @Column(name = "last_name",nullable = false)
+    private String lastName;
+    @Column(name = "email_id",nullable = false)
+    private String email;
+    @Column(name = "Department",nullable = false)
+    private String Department;
+    @Column(name = "yearOfEnrollment",nullable = false)
+    private long yearOfEnrollment;
+   
+   
+       
+}
